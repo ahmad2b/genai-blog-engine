437944c57c4eb9831e8b2e23de91cbf92d7b1506
Muhammad Ahmad <ahmadshaukat_4@outlook.com>
2024-03-04 11:02:27 +0500

    Update README.md and comment out maxDuration variable

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { formatDate } from '@/lib/utils'
 import Form from '@/components/form'
 import { Card, CardContent } from '@/components/ui/card'
 
-export const maxDuration = 60
+// export const maxDuration = 60
 
 export default async function Home() {
   const blogs = await getAllBlogs()
